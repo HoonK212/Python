00eae89e19b31430279883c16d0d981fa6a7f943
Hoon Kim <b0aeiou@gmail.com>
2020-10-22 19:56:26 +0900

    파이썬 기본 - 자료형

---
+++ practice.py
@@ -0,0 +1,72 @@
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+3)
+print(2*8)
+print(3*(3+1))
+
+print("=== === ===")
+
+print('풍선')
+print("나비")
+print("ㅋㅋㅋㅋㅋㅋㅋ")
+print("ㅋ"*7)
+
+print("=== === ===")
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+print("=== === ===")
+
+# 애완동물을 소개해 주세요.
+
+animal = "강아지"
+name = "남빵이"
+age = 1
+like = "사람"
+is_adult = age >= 3
+
+animal = "고양이"
+name = "냥망고"
+age = 1
+like = "누나"
+is_adult = age >= 3
+
+'''
+print("우리집 " + animal + "의 이름은 " + name + "에요.")
+print(name + "는 " + str(age) + "살이고 " + like + "을(를) 아주 좋아해요.")
+print(name + "는 어른일까요? " + str(is_adult))
+'''
+
+print("우리집 ", animal, "의 이름은 ", name, "에요.")
+print(name, "는 ", age, "살이고 ", like, "을(를) 아주 좋아해요.")
+print(name, "는 어른일까요? ", is_adult)
+
+print("=== === ===")
+
+# Quiz) 변수를 이용하여 다음 문장을 출력하시오.
+
+# 변수명
+#   : station
+
+# 변수값
+#   : "사당", "신도림", "인천공항" 순서대로 입력
+
+# 출력 문장
+#   : XX 행 열차가 들어오고 있습니다.
+
+station = "사당"
+print(station, "행 열차가 들어오고 있습니다.")
+
+station = "신도림"
+print(station, "행 열차가 들어오고 있습니다.")
+
+station = "인천공항"
+print(station, "행 열차가 들어오고 있습니다.")
